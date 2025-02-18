4df3608a4c78e885e06cf3a781452a15a066244b
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-02-18 15:02:56 +0000

    update OAuth2 local flow for admin callback page

--- src/pages/AdminCallbackPage/AdminCallbackPage.tsx
+++ src/pages/AdminCallbackPage/AdminCallbackPage.tsx
@@ -1,19 +1,38 @@
-import { useState, useMemo } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { useInitialisedDeskproAppClient } from "@deskpro/app-sdk";
+import { FC, useState } from 'react';
+import { useDeskproLatestAppContext, useInitialisedDeskproAppClient } from '@deskpro/app-sdk';
 import { AdminCallback } from "../../components";
-import type { FC } from "react";
-import type { Maybe } from "../../types";
+import type { Maybe, Settings } from '../../types';
+import { AUTH_URL } from '../../constants';
+import { getQueryParams } from '../../utils';
 
 const AdminCallbackPage: FC = () => {
+  const { context } = useDeskproLatestAppContext<unknown, Settings>();
   const [callbackUrl, setCallbackUrl] = useState<Maybe<string>>(null);
-  const key = useMemo(() => uuidv4(), []);
 
-  useInitialisedDeskproAppClient((client) => {
-    client.oauth2()
-      .getAdminGenericCallbackUrl(key, /code=(?<token>[0-9a-f]+)/, /state=(?<key>.+)/)
-      .then(({ callbackUrl }) => setCallbackUrl(callbackUrl));
-  }, [key]);
+  useInitialisedDeskproAppClient(client => {
+    const clientID = context?.settings.client_id;
+
+    client.startOauth2Local(
+      ({ callbackUrl, state }) => {
+        setCallbackUrl(callbackUrl);
+
+        return `${AUTH_URL}/authorization?${getQueryParams({
+          client_id: clientID ?? '',
+          state,
+          response_type: 'code',
+          code_challenge_method: 'S256',
+          code_challenge: '',
+          redirect_uri: callbackUrl,
+        })}`;
+      },
+      /code=(?<code>[0-9a-f]+)/,
+      async () => ({data: {access_token: ''}}),
+      {
+        pollInterval: 10000,
+        timeout: 600
+      }
+    );
+  }, []);
 
   return (
     <AdminCallback callbackUrl={callbackUrl} />
